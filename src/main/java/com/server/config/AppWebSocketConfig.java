1e8a3e06430ccbb3465ab188d8e6a9470ba0051e
Alexey <Alexey007gold@mail.ru>
2017-04-01 20:50:34 +0300

    Second commit

--- src/main/java/com/server/config/AppWebSocketConfig.java
+++ src/main/java/com/server/config/AppWebSocketConfig.java
@@ -13,7 +13,7 @@ public class AppWebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer
 	@Override
 	public void configureMessageBroker(MessageBrokerRegistry config) {
 		config.enableSimpleBroker("/topic");
-		config.setApplicationDestinationPrefixes("");
+		config.setApplicationDestinationPrefixes("/app");
 	}
 
 	@Override
